f2640d752a6e0223c05b4de599fc0845211ad6a3
Ben Cavins <ben.cavins@flatironschool.com>
2023-01-15 10:58:49 -0500

    first two deliverabled

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code here...
+let firstMenuItem;
+
+fetch('http://localhost:3000/menu')
+    .then(resp => resp.json())
+    .then(menuItems => menuItems.forEach(menuItem => {
+        firstMenuItem = menuItems[0]
+
+        const menuItemSpan = document.createElement('span')
+        menuItemSpan.textContent = menuItem.name
+        
+        document.querySelector('#menu-items').append(menuItemSpan)
+    }))
+    .then(() => {
+        // TODO add to screen
+        renderMenuItem(firstMenuItem)
+    })
+
+function renderMenuItem(menuItem) {
+    const img = document.querySelector('#dish-image')
+    img.src = menuItem.image
+
+    const dishH3 = document.querySelector('#dish-name')
+    dishH3.textContent = menuItem.name
+
+    const dishDesc = document.querySelector('#dish-description')
+    dishDesc.textContent = menuItem.description
+
+    const dishPrice = document.querySelector('#dish-price')
+    dishPrice.textContent = menuItem.price
+}
