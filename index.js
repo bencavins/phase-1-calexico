600b03fe2b9a23946cab1421f6d9b86c7250ff43
Ben Cavins <ben.cavins@flatironschool.com>
2023-08-28 15:57:06 -0400

    do deliv 1-3

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code here...
+const menuItemsDiv = document.querySelector('#menu-items')
+const dishImg = document.querySelector('#dish-image')
+const dishName = document.querySelector('#dish-name')
+const dishDesc = document.querySelector('#dish-description')
+const dishPrice = document.querySelector('#dish-price')
+
+fetch("http://localhost:3000/menu")
+.then(resp => resp.json())
+.then(data => {
+    // loop through menu items
+    data.forEach(menuItem => {
+        // create span
+        const span = document.createElement('span')
+        // put name in span
+        span.textContent = menuItem.name
+        // attach span to div
+        menuItemsDiv.append(span)
+
+        span.addEventListener('click', event => {
+        })
+    })
+
+    // render first menu item
+    renderMenuItem(data[0])
+})
+
+function renderMenuItem(menuItem) {
+    dishImg.src = menuItem.image
+    dishName.textContent = menuItem.name
+    dishDesc.textContent = menuItem.description
+    dishPrice.textContent = menuItem.price
+}
